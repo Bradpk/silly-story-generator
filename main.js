9d6e2a122940111799b051ce4f43c9d7b3ef8374
Brad Kerr <brad.pk@live.com>
2023-05-31 15:19:08 +0000

    debugging

--- main.js
+++ main.js
@@ -36,9 +36,9 @@ function result() {
 
   if(document.getElementById("uk").checked) {
     const weight = Math.round(300 / 14) + 'stone';
-    const temperature =  Math.round(94 - 32 / 1.8) + 'celcius';
+    const temperature =  Math.round((94 - 32) * 5/9) + 'celcius';
     const newStory = newStory.replace('300 pounds', weight);
-    const newStory - newStory.replace('94 fahrenheit', temperature);
+    const newStory = newStory.replace('94 fahrenheit', temperature);
 
   }
 
