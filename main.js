a99bc87bd12610b5586b852d5d12715c794132e2
Brad Kerr <brad.pk@live.com>
2023-05-31 15:30:59 +0000

    g

--- main.js
+++ main.js
@@ -7,25 +7,24 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-const storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.'
-const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas']
-const insertY = ['the soup kitchen', 'Disneyland', 'the White House']
-const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away']
 
-
+let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.'
+let insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas']
+let insertY = ['the soup kitchen', 'Disneyland', 'the White House']
+let insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away']
 
 randomize.addEventListener('click', result);
 
 function result() {
-    const newStory = storyText;
-    const xItem = randomValueFromArray(insertX);
-    const yItem = randomValueFromArray(insertY);
-    const zItem = randomValueFromArray(insertZ);
+    let newStory = storyText;
+    let xItem = randomValueFromArray(insertX);
+    let yItem = randomValueFromArray(insertY);
+    let zItem = randomValueFromArray(insertZ);
 
-    const newStory = newStory.replace(':insertx:', xItem);
-    const newStory = newStory.replace(':inserty:', yItem);
-    const newStory = newStory.replace(':insertz:', zItem);
-    const newStory = newStory.replace(':insertx:', xItem);
+    let newStory = newStory.replace(':insertx:', xItem);
+    let newStory = newStory.replace(':inserty:', yItem);
+    let newStory = newStory.replace(':insertz:', zItem);
+    let newStory = newStory.replace(':insertx:', xItem);
 
 
   if(customName.value !== '') {
@@ -37,8 +36,8 @@ function result() {
   if(document.getElementById("uk").checked) {
     const weight = Math.round(300 / 14) + 'stone';
     const temperature =  Math.round((94 - 32) * 5/9) + 'celcius';
-    const newStory = newStory.replace('300 pounds', weight);
-    const newStory = newStory.replace('94 fahrenheit', temperature);
+    let newStory = newStory.replace('300 pounds', weight);
+    let newStory = newStory.replace('94 fahrenheit', temperature);
 
   }
 
